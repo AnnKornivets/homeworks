b1155882c9d60558f07da20d132ab0ac96cd613b
annakornivec <kona.96@inbox.ru>
2019-03-12 18:44:09 +0300

    extra

---
+++ src/homework2/military.java
@@ -0,0 +1,8 @@
+package homework2;
+
+public class military {
+    public static void main(String[] args) {
+        int b=0;
+
+    }
+}
---
+++ src/homework2/non_negativeNumbers.java
@@ -0,0 +1,10 @@
+package homework2;
+
+public class non_negativeNumbers {
+    public static void main(String[] args) {
+        int a ;
+        for(a=95;a>0;a-=5){
+            System.out.println(a+"");
+        }
+    }
+}
